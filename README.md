b272846195a89536e97f6740b97eff0d769f8eb3
Gitesh Sharma <58145476+Glitchedgitz@Users.Noreply.Github.Com>
2024-04-24 19:13:55 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Svelte + Typescript + Tailwind Template
+
+```
+git clone https://github.com/glitchedgitz/svelte-ts-tailwind-template
+cd svelte-ts-tailwind-template
+npm install
+npm run dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div
+	class="relative left-1/2 top-1/2 flex h-fit w-fit -translate-x-1/2 -translate-y-1/2 gap-4 font-medium opacity-80"
+>
+	Svelte
+	<p class="opacity-40">+</p>
+	TypeScript
+	<p class="opacity-40">+</p>
+	Tailwind
+</div>
